33fd7544d43ca3c9e96a2c047f9c6f16854d6124
KeparYTbcc <67996155+KeparYTbcc@users.noreply.github.com>
2021-01-21 01:30:48 +0000

    Update and rename Wifi__Csdl android wifi school password bypass.bat to bypass.sh

--- bypass.sh
+++ bypass.sh
@@ -1,41 +1,8 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFBZNARSBbleeCaIS5Of66/m7lU4OUO5/T4bd0/SMOfAS+VbYQpU+0nZlr8IKBSR6ZwKqfRwxuyBHrmHl
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 color a
-cls
+clear
 title Wifi Csdl Android Wifi school password bypass
-cls
+clear
 if exist adb.zip (
     goto start
 ) else (
@@ -43,7 +10,7 @@ if exist adb.zip (
 )
 :start
 msg * SvP branche votre telephone a cette ordinateur, N'OUBLIER PAS D'ACTIVE "USB DEBUGGING": http^:^/^/bit.ly^/3p3DOoX
-cls
+clear
 echo Quand Vous Serai Pret A Continuer,
 echo Que Votre Telephone Est Bien Branche Depuis Au Moin Une Minute
 echo Que L'option "Debogage USB" Est Bien ACTIVE
